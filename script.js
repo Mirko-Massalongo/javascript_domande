4e4a9665ed5988cddc24035e958a4a067e02e8c3
mirko-massalong <mirkomassa7499@gmail.com>
2023-10-12 18:46:51 +0200

    primo compito javascript

---
+++ script.js
@@ -0,0 +1,7 @@
+// domande per il compito
+// un algoritmo è sono delle istruzioni per risolvere dei problemi ,e ottenere un risultato usando dei numeri di dati iniziali
+
+// una variabile è un contenitore come una scodella e gli si da il nome al determinato contenitore cioè la scodella ed è composta da numeri o caratteri
+
+//undefined e null non sono la stessa cosa undefined significa che non c'è nessuna informazione nel senso che la varibile non è specificata 
+// null invece signica è un modo specifico per dare una informazione significa che la variabile non ha valore cioè numeri o caratteri non so stati assegnati
